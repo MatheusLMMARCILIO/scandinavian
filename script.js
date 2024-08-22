ecf22120d5da54018fe741924518791b39a844d6
Matheus Luiz <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-08-22 17:24:15 -0300

    22/08/2024 17h24

---
+++ script.js
@@ -0,0 +1,32 @@
+const btnImageBox = document.querySelector('.btnImageBox')
+const imgImageBtn = document.querySelector('.imgImageBtn')
+
+imgImageBtn.addEventListener("mouseover", () => {
+    btnImageBox.style.display = 'flex'
+})
+
+imgImageBtn.addEventListener('mouseout', () => {
+    btnImageBox.style.display = 'none'
+})
+
+const btnImageBox2 = document.querySelector('.btnImageBox2')
+
+const imgImageBtn2 = document.querySelector('.imgImageBtn2')
+
+imgImageBtn2.addEventListener("mouseover", () => {
+    btnImageBox2.style.display = 'flex'
+})
+
+imgImageBtn2.addEventListener('mouseout', () => {
+    btnImageBox2.style.display = 'none'
+})
+const btnImageBox3 = document.querySelector('.btnImageBox3')
+const imgImageBtn3 = document.querySelector('.imgImageBtn3')
+
+imgImageBtn3.addEventListener("mouseover", () => {
+    btnImageBox3.style.display = 'flex'
+})
+
+imgImageBtn3.addEventListener('mouseout', () => {
+    btnImageBox3.style.display = 'none'
+})
