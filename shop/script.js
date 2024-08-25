a428b523dbaf551893246d8ac138017f729481a3
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-08-25 00:10:26 -0300

    25/08/2024 00h10

---
+++ shop/script.js
@@ -0,0 +1,61 @@
+const btnImageBox = document.querySelector('.btnImageBox')
+const imgImageBtn = document.querySelector('.box')
+const btnImageBox2 = document.querySelector('.btnImageBox2')
+const imgImageBtn2 = document.querySelector('.box2')
+const btnImageBox3 = document.querySelector('.btnImageBox3')
+const imgImageBtn3 = document.querySelector('.box3')
+const btnImageBox4 = document.querySelector('.btnImageBox4')
+const imgImageBtn4 = document.querySelector('.box4')
+const btnImageBox5 = document.querySelector('.btnImageBox5')
+const imgImageBtn5 = document.querySelector('.box5')
+const btnImageBox6 = document.querySelector('.btnImageBox6')
+const imgImageBtn6 = document.querySelector('.box6')
+
+imgImageBtn.addEventListener("mouseover", () => {
+    btnImageBox.style.display = 'flex'
+})
+
+imgImageBtn.addEventListener('mouseout', () => {
+    btnImageBox.style.display = 'none'
+})
+
+imgImageBtn2.addEventListener("mouseover", () => {
+    btnImageBox2.style.display = 'flex'
+})
+
+imgImageBtn2.addEventListener('mouseout', () => {
+    btnImageBox2.style.display = 'none'
+})
+
+imgImageBtn3.addEventListener("mouseover", () => {
+    btnImageBox3.style.display = 'flex'
+})
+
+imgImageBtn3.addEventListener('mouseout', () => {
+    btnImageBox3.style.display = 'none'
+})
+
+imgImageBtn4.addEventListener("mouseover", () => {
+    btnImageBox4.style.display = 'flex'
+})
+
+imgImageBtn4.addEventListener('mouseout', () => {
+    btnImageBox4.style.display = 'none'
+})
+
+imgImageBtn5.addEventListener("mouseover", () => {
+    btnImageBox5.style.display = 'flex'
+})
+
+imgImageBtn5.addEventListener('mouseout', () => {
+    btnImageBox5.style.display = 'none'
+})
+
+
+imgImageBtn6.addEventListener("mouseover", () => {
+    btnImageBox6.style.display = 'flex'
+})
+
+imgImageBtn6.addEventListener('mouseout', () => {
+    btnImageBox6.style.display = 'none'
+})
